197805bfa868a9ea11a01030c8c41737a4c6950b
ochlo <17635151+ochlo@users.noreply.github.com>
2017-08-06 23:44:23 -0700

    Update setup.sh

--- ez-Elist/setup.sh
+++ ez-Elist/setup.sh
@@ -6,6 +6,7 @@ installDir=$3
 
 mkdir $serverDir/$listName/add/ $serverDir/$listName/del/
 ln -s $instalDir/html.php $serverDir/$listName/index.php
+ln -s $instalDir/db.php $serverDir/$listName/db.php
 ln -s $instalDir/add.php $serverDir/$listName/add/index.php
 ln -s $instalDir/del.php $serverDir/$listName/del/index.php
 echo '<?php $pageId = "List";' > $serverDir/$listName/config.php
