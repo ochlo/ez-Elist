69d3f197b4aa2759ed5e82954eb628b14cbbcb10
ochlo <17635151+ochlo@users.noreply.github.com>
2017-08-07 17:45:41 -0700

    Update html.php

--- ez-Elist/html.php
+++ ez-Elist/html.php
@@ -6,39 +6,7 @@ include 'db.php';
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <style type='text/css'>
-        body {
-            background:     #000;
-            font-family:    arial;
-            font-size:      x-large;
-        }
-        td {
-            background:     #FFF;
-            border-style:   outset;
-            border-width:   3px;
-        }
-        th {
-            background:     #000;
-            border-style:   inset;
-            border-width:   3px;
-            color:          #FFF;
-            font-weight:    bold;
-            letter-spacing: 5px;
-            text-align:     center;
-        }
-        a:link {
-            color:          #000;
-        }
-        a:visited {
-            color:          #000;
-        }
-        a:hover {
-            color:          #FF0000;
-        }
-        a:active {
-            color:          #FFF;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>
         <?php echo $pageId ?>
     </title>
