36e4476be7344e773b9eed7a3502f776fb5af08c
ochlo <17635151+ochlo@users.noreply.github.com>
2017-08-07 17:39:34 -0700

    Update setup.sh

--- ez-Elist/setup.sh
+++ ez-Elist/setup.sh
@@ -3,13 +3,20 @@
 listName=$1
 serverDir=$2
 installDir=$3
+usage() {
+    printf 'Usage: %s ListName /Server/Dir /Install/Dir
+    ListName:  Name of your list
+    ServerDir: Web server directory to install the list files
+    InstalDir: Directory in which base installation files will reside\n'\
+    "$(basename "$0")" >&2;
+}
 
 if [ $# -lt 3 ]; then
-    printf 'Usage: %s ListName ServerDir InstallDir ...\n' "$(basename "$0")" >&2
-    exit 64
+    usage; exit 64;
+#elif [  ]; then
 fi
 
-mkdir $serverDir/$listName/add/ $serverDir/$listName/del/
+mkdir $serverDir/$listName $serverDir/$listName/add/ $serverDir/$listName/del/
 ln -s $instalDir/html.php $serverDir/$listName/index.php
 ln -s $instalDir/db.php $serverDir/$listName/db.php
 ln -s $instalDir/add.php $serverDir/$listName/add/index.php
