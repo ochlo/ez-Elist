bfe62c282b9828cd1bb68a84bcb01a6921ec381a
ochlo <17635151+ochlo@users.noreply.github.com>
2017-08-07 17:16:52 -0700

    Update setup.sh

--- ez-Elist/setup.sh
+++ ez-Elist/setup.sh
@@ -4,6 +4,11 @@ listName=$1
 serverDir=$2
 installDir=$3
 
+if [ $# -lt 3 ]; then
+    printf 'Usage: %s ListName ServerDir InstallDir ...\n' "$(basename "$0")" >&2
+    exit 64
+fi
+
 mkdir $serverDir/$listName/add/ $serverDir/$listName/del/
 ln -s $instalDir/html.php $serverDir/$listName/index.php
 ln -s $instalDir/db.php $serverDir/$listName/db.php
